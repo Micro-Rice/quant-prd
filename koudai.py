346fd068b8b360c1851a11f08bfee0b86e953b84
kk2 <635435791@qq.com>
2019-11-21 20:37:29 +0800

    second commit

--- koudai.py
+++ koudai.py
@@ -1,5 +1,20 @@
+
+
 import jqdatasdk as jq
 import datetime as dt
+
+
 jq.auth("13811944446", "1qazxsw2")
 
 print(jq.__version__)
+if __name__ == '__main__':
+stocks = ['600000.XSHG', '000001.XSHE']
+
+stocks.remove('688368.XSHG')
+stocks.remove('688138.XSHG')
+
+now = dt.datetime.now()
+today = now.strftime('%Y-%m-%d')
+
+panel = jq.get_price(stocks, count=60, end_date=today, frequency='daily', fields=['close'])
+
