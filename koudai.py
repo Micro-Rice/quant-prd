8d8984f906e0627f887acd7507d9f5dafb6d7909
kk2 <635435791@qq.com>
2019-11-23 00:28:47 +0800

    modify bug

--- koudai.py
+++ koudai.py
@@ -5,6 +5,7 @@ import datetime as dt
 def ma_func(pd, n):
     sum_c = 0
     pd_t = pd.tail(n)
+    print(pd_t)
     for i in range(n):
         sum_c = sum_c + pd_t.iat[i]
     avg = round(sum_c / n, 2)
@@ -18,11 +19,12 @@ if __name__ == '__main__':
     stock_SH = jq.get_index_stocks('000001.XSHG')
     stock_SZ = jq.get_index_stocks('399001.XSHE')
     stock_cy = jq.get_index_stocks('399006.XSHE')
-    stocks = stock_SZ + stock_cy
+    stocks = stock_SZ + stock_SH + stock_cy
+    stocks = list(set(stocks))
     # print(len(stocks))
-    # stocks = ['300001.XSHE']
+    # stocks = ['003816.XSHE']
 
-    stocks.remove('300001.XSHE')
+    # stocks.remove('300001.XSHE')
     # stocks.remove('688138.XSHG')
 
     today = dt.date.today()
@@ -33,7 +35,7 @@ if __name__ == '__main__':
     for stock in stocks:
         dfc = df_close[stock]
         dfv = df_vol[stock]
-        print(stock)
+        # print(stock)
         ma5 = ma_func(dfc, 5)
         ma10 = ma_func(dfc, 10)
         ma20 = ma_func(dfc, 20)
