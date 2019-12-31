f60cc50c7ddae3fa64ad7afe99457abcaf8caad5
zhengyi_it <zhengyi_it@chinastock.com.cn>
2019-12-31 13:47:54 +0800

    my test gitmail

--- koudai.py
+++ koudai.py
@@ -1,5 +1,8 @@
 import jqdatasdk as jq
 import datetime as dt
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 
 def ma_func(pd, n):
@@ -10,6 +13,33 @@ def ma_func(pd, n):
         sum_c = sum_c + pd_t.iat[i]
     avg = round(sum_c / n, 2)
     return avg
+
+
+def mail_func(info):
+    # 第三方 SMTP 服务
+    mail_host = "smtp.126.com"  # 设置服务器
+    mail_user = "zhengy19881123"  # 用户名
+    mail_pass = "19881123"  # 口令
+
+    sender = 'zhengy19881123@126.com'
+    receivers = ['635435791@qq.com']
+
+    mail_msg = info
+    message = MIMEText(mail_msg, 'html', 'utf-8')
+    message['From'] = Header(sender, 'utf-8')
+    message['To'] = Header(receivers, 'utf-8')
+
+    subject = '生成的股票列表'
+    message['Subject'] = Header(subject, 'utf-8')
+
+    try:
+        smtpObj = smtplib.SMTP()
+        smtpObj.connect(mail_host, 25)  # 25 为 SMTP 端口号
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print("邮件发送成功")
+    except smtplib.SMTPException:
+        print("Error: 无法发送邮件")
 
 
 if __name__ == '__main__':
@@ -59,4 +89,7 @@ if __name__ == '__main__':
             # print(close)
             # print(max_c)
             # print(stock)
-    print(res_sts)
+    res = ",".join(res_sts)
+    print(res)
+    mail_func(res)
+
